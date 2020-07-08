7f5e025cf51750324d00fbed06677db83f8f4eac
kerovinoddi <kerovineoddi@gmail.com>
2020-07-08 14:56:21 +0300

    added macros.html file

--- app/request.py
+++ app/request.py
@@ -1,9 +1,9 @@
 from app import app
 import urllib.request,json
-from models import news
+from models import Succes,Articles
+from datetime import datetime
 
 
-News = news.News
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 

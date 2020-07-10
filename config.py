f5673948e7c3dc3a7ba74caab1fcdbce4a0b16ef
kerovinoddi <kerovineoddi@gmail.com>
2020-07-10 08:08:13 +0300

    installed heroku cli

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
+   import os
 
+   class Config:
 class Config:    
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
-
+   class Config:
+    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+   	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+   	@staticmethod
+   	def init_app(app):
+   		pass
+    
+    class ProdConfig(Config):
     pass
 
-class ProdConfig(Config):
-    '''
-    Producyion configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
 
-class DevConfig(Config):
-    '''
-    Development configuration child class
-    Args:
-        Config: The parent configuration class with General configuration setting
-    '''
+    class DevConfig(Config):
+        DEBUG = True
 
-    DEBUG = True
+
+    config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
+
+
 
